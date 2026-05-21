e5c70d08131ae6a5d1b1c65d246c0761eb277359
brunosouza <bruno.souza@onfly.com.br>
2026-05-20 22:57:22 -0300

    fix(config): allow both CLOUDINARY_NAME and NUXT_CLOUDINARY_CLOUD_NAME

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   modules: ['nuxt-auth-utils', '@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/cloudinary', '@vercel/speed-insights'],
   
   cloudinary: {
-    cloudName: process.env.CLOUDINARY_NAME
+    cloudName: process.env.CLOUDINARY_NAME || process.env.NUXT_CLOUDINARY_CLOUD_NAME
   },
 
   app: {
